2ff6cb20364c04f2bc04321c1539656e0470d117
vjarne-periel <vincent.jarne@live.fr>
2026-07-11 12:40:42 +1100

    Update main.py

--- main.py
+++ main.py
@@ -7,11 +7,30 @@ log.logging()
 st.image("img/Zero white title.png", width="stretch")
 
 "# Zero licenses"
-st.caption("Generate a new license", text_alignment="center")
+st.caption("GENERATE A NEW LICENSE", text_alignment="center")
 
-c = st.columns(2)
-fname = c[0].text_input("First name", "")
-name = c[1].text_input("Name", "")
+with st.form("new lic") :
+  st.caption("Geologist", text_alignment="center")
+  c = st.columns(2)
+  fname = c[0].text_input(":material/person: First name", "")
+  name = c[1].text_input(":material/person: Name", "")
+  cie = st.text_input(":material/person: Company", "")
+  
+  st.caption("License", text_alignment="center")
+  c = st.columns(2)
+  license_type = c[0].selectbox(":material/license: License type", ["PC","server"])
+  license_cover = c[1].selectbox(":material/license: License cover", ["Trial","Premium"])
+  expiry = st.date_input("Expiry", "today", format="YYYY-MM-DD")
+  st.form_submit_button(":material/check: Submit", type="primary")
 
-cie = c[0].text_input("Company", "")
-expiry = c[1].date_input("Expiry", "today")
+license_data = {
+    "geologist_fname": fname,
+    "geologist_name" : name,
+    "geologist_cie"  : cie,
+    "expires": expiry,
+    "license_type" : license_type,
+    "license_cover" : license_cover,
+}
+
+key = st.file_uploader("Private password key", type=["pem"])
+
