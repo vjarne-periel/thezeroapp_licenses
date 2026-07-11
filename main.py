589978afc35e2f62fd5474a957ca49cdbea5eb72
vjarne-periel <vincent.jarne@live.fr>
2026-07-11 12:19:00 +1100

    Update main.py

--- main.py
+++ main.py
@@ -5,4 +5,11 @@ import log
 log.logging()
 
 "# Zero licenses"
+"Generate a new license"
 
+c = st.columns(2)
+fname = c[0].text_input("First name", "")
+name = c[1].text_input("Name", "")
+
+cie = c[0].text_input("Company", "")
+expiry = c[1].date_input("Expiry", "today")
