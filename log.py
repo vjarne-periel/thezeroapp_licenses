2ca39c11dad1c47f7f9b6c1bd597efeb59190ddd
vjarne-periel <vincent.jarne@live.fr>
2026-07-11 11:58:35 +1100

    Create log.py

---
+++ log.py
@@ -0,0 +1,29 @@
+import streamlit as st
+
+
+def logging() :
+  if "access" not in st.session_state :
+    st.session_state.access = False
+    st.session_state.count = 0
+    
+  if st.session_state.access == True :
+    return
+
+  modal_pw()
+
+
+@st.dialog("Log")
+def modal_pw() :
+  if st.session_state.count >= 3 :
+    st.error("Too mony tries")
+
+  
+  with st.form("pws") :
+    pw1 = st.text_input("Password 1", None, type="password")
+    pw2 = st.text_input("Password 2", None, type="password")
+    if st.form_submit_button(":material/check: Submit", type="primary") :
+      if pw1 == st.secrets["pw1"] and pw2 == st.secrets["pw2"] :
+        st.session_state.access = True
+      else :
+        st.error("Wrong passwords")    
+  
